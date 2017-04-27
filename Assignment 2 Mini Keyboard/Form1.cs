d22903b980af9620b03e897444767b2e254ddde9
Nicolas Giurlani <b6029664@my.shu.ac.uk>
2017-04-28 00:36:50 +0100

    Completed Button 4 for Task 4
    - Used the same code as previous, modified values in order for it to
    function with button 4.

--- Assignment 2 Mini Keyboard/Form1.cs
+++ Assignment 2 Mini Keyboard/Form1.cs
@@ -261,9 +261,56 @@ namespace Assignment_2_Mini_Keyboard
         private void btn_4_Click(object sender, EventArgs e)
         {
             withinTimer.Interval = intIntervalRequired;
-            withinTimer.Start();
-            rtxt_WordBuilder.Text += "g";
-            Str_KeyStrokes += "4";
+            if (!withinTimer.Enabled)
+            {
+                rtxt_WordBuilder.Text += "g";
+                Str_KeyStrokes += "4";
+                withinTimer.Start();
+            }
+            else
+            {
+                string lastChar = "";
+
+                // Gets the last character
+                if (rtxt_WordBuilder.Text.Length > 0)
+                {
+                    lastChar = rtxt_WordBuilder.Text.Substring(rtxt_WordBuilder.Text.Length - 1, 1);
+                }
+
+
+                // Deletes the last character
+                rtxt_WordBuilder.Text = rtxt_WordBuilder.Text.Substring(0, rtxt_WordBuilder.Text.Length - 1);
+
+                // reset timer
+                withinTimer.Stop();
+                withinTimer.Start();
+                // depending on last char, pick new one
+                switch (lastChar)
+                {
+                    case "g":
+                        rtxt_WordBuilder.Text += "h";
+                        break;
+                    case "h":
+                        rtxt_WordBuilder.Text += "i";
+                        break;
+                    case "i":
+                        rtxt_WordBuilder.Text += "4";
+                        break;
+                    case "4":
+                        rtxt_WordBuilder.Text += "G";
+                        break;
+                    case "G":
+                        rtxt_WordBuilder.Text += "H";
+                        break;
+                    case "H":
+                        rtxt_WordBuilder.Text += "I";
+                        break;
+                    case "I":
+                        rtxt_WordBuilder.Text += "g";
+                        break;
+
+                }
+            }
         }
 
         private void btn_5_Click(object sender, EventArgs e)
